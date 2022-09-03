b3a67a05b9dce078444f29687c145412a6db2374
Tony Giorgio <tonygiorgio@protonmail.com>
2022-09-03 11:50:05 -0500

    Get request

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,11 @@
-use tor_example_rs::create_owned_tor_service;
+use tor_example_rs::{create_owned_tor_service, get_request};
 
 pub fn main() {
     let _tor_service = create_owned_tor_service();
 
     println!("Tor Created!!\n Socks5 Proxy: 127.0.0.1:{}\n", 19054);
+
+    let get_req = get_request();
+
+    println!("make a get request from library: {}", get_req);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,33 @@
+use std::io::prelude::*;
 use tor::{OwnedTorService, TorService, TorServiceParam};
+use tor_stream::TorStream;
 
 pub fn create_owned_tor_service() -> OwnedTorService {
     let tor_service = create_tor_service();
     let owned_tor_service = tor_service.into_owned_node().unwrap();
     return owned_tor_service;
+}
+
+pub fn get_request() -> String {
+    let mut stream = TorStream::connect("www.example.com:80").expect("Failed to connect");
+
+    // The stream can be used like a normal TCP stream
+
+    stream
+        .write_all(b"GET / HTTP/1.1\r\nConnection: Close\r\nHost: www.example.com\r\n\r\n")
+        .expect("Failed to send request");
+
+    // If you want the raw stream, call `into_inner()`
+
+    let mut stream = stream.into_inner();
+
+    let mut buf = String::new();
+    stream
+        .read_to_string(&mut buf)
+        .expect("Failed to read response");
+
+    println!("Server response:\n{}", buf);
+    return buf;
 }
 
 pub fn create_tor_service() -> TorService {
